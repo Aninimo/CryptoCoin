3ae01edbcfd6f669bbebcdd009fd021e1a8a9c21
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-07-02 21:58:07 -0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Routing } from './routes'
+
+import './global.css'
+
+export default function App() {
+  return (
+     <main className='h-auto bg-slate-800 text-white'>
+       <Routing/>
+     </main>
+  )
+}
