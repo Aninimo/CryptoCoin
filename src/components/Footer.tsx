fee66071e4b3954e557965ca2cc0fab731c2f48d
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-07-03 06:38:11 -0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer(){
+  return(
+    <footer>
+      <img src='https://raw.githubusercontent.com/stefvndev/coindom-crypto/main/src/images/footer/footer-bg.png'/>
+    </footer>
+  )
+}
