52532693fab621bd9f5dd9769c56684aba3d10fb
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-07-02 22:15:44 -0300

    Create JoinUs.tsx

---
+++ src/components/JoinUs.tsx
@@ -0,0 +1,15 @@
+export function JoinUs(){
+  return(
+    <div className='flex flex-col justify-center items-center mt-12'>
+      <h1 className='font-bold text-2xl mb-4'>
+        JOIN US
+      </h1>
+      <p>
+        Invest and manage all your crypto at one place.
+      </p>
+      <button className='bg-indigo-800 rounded-3xl text-white p-4 px-24 mt-4'>
+        Join us
+      </button>
+    </div>
+  )
+}
